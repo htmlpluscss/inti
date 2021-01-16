d67022363cfab50b18df596fbcb7ce743ddec1cd
htmlpluscss <79198889134@ya.ru>
2021-01-16 21:20:00 +0300

    Мои сертификации

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -8,27 +8,6 @@
 
 	Array.from(tabs, tab => {
 
-		tab.addEventListener('click', event => {
-
-			if(event.target.closest('.tabs__btn')) {
-
-				event.preventDefault();
-
-				const btn = event.target.closest('.tabs__btn'),
-					  btns = tab.querySelectorAll('.tabs__btn'),
-					  items = tab.querySelectorAll('.tabs__item');
-
-				Array.from(btns, (_btn, index) => {
-
-					_btn.classList.toggle('is-active', _btn === btn);
-					items[index].classList.toggle('is-active', _btn === btn);
-
-				});
-
-			}
-
-		});
-/*
 		const btns = tab.querySelectorAll('.tabs__btn'),
 			  items = tab.querySelectorAll('.tabs__item');
 
@@ -49,6 +28,6 @@
 
 		});
 
-*/	});
+	});
 
 })(document.querySelectorAll('.tabs'));
