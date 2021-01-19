f692f952767b3ff7903164d0fcc9a83b5d6da385
htmlpluscss <79198889134@ya.ru>
2021-01-19 15:49:09 +0300

    tab hash

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -6,27 +6,38 @@
 
 	}
 
+	// проверяем hash
+	const hash = location.hash;
+
+	const btnHash = (btns,items) => {
+
+		Array.from(btns, (_btn, index) => setActive(_btn, items[index], _btn.href === location.href));
+
+	};
+
+	const setActive = (btn, item, toggle) => {
+
+		btn.classList.toggle('is-active', toggle);
+		item.classList.toggle('is-active', toggle);
+
+	}
+
 	Array.from(tabs, tab => {
 
 		const btns = tab.querySelectorAll('.tabs__btn'),
 			  items = tab.querySelectorAll('.tabs__item');
 
-		Array.from(btns, btn => {
+		Array.from(btns, btn =>
+			btn.addEventListener('click', event =>
+				Array.from(btns, (_btn, index) =>
+					setActive(_btn, items[index], _btn === btn))));
 
-			btn.addEventListener('click', event => {
+		if(hash) {
 
-				event.preventDefault();
+			btnHash(btns,items);
+			window.addEventListener('hashchange', () => btnHash(btns,items));
 
-				Array.from(btns, (_btn, index) => {
-
-					_btn.classList.toggle('is-active', _btn === btn);
-					items[index].classList.toggle('is-active', _btn === btn);
-
-				});
-
-			});
-
-		});
+		}
 
 	});
 
