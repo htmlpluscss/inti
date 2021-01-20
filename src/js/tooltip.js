9cb318a32cbe030e921135bf215fc6f608452f4f
htmlpluscss <79198889134@ya.ru>
2021-01-21 01:05:09 +0300

    chat

--- src/js/tooltip.js
+++ src/js/tooltip.js
@@ -22,10 +22,12 @@
 
 	if(tooltips.length){
 
-		const ico = document.createElement('span');
+		const ico = document.createElement('span'),
+			  icoChat = document.createElement('span');
 
 		ico.innerHTML = '<svg viewBox="0 0 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M20 12a8 8 0 11-16 0 8 8 0 0116 0zm-5.74-3.87a3.18 3.18 0 01.56 3.78c-.38.7-1 1.22-1.74 1.49a.4.4 0 00-.28.38v.62a.8.8 0 01-1.6 0V12.8A.8.8 0 0112 12c.88 0 1.6-.72 1.6-1.6a1.6 1.6 0 00-3.2 0 .8.8 0 01-1.59.08l-.01-.12c.01-2 1.86-3.55 3.95-3.07.57.13 1.1.42 1.51.84zM12.8 16.8a.8.8 0 11-1.6 0 .8.8 0 011.6 0z"/></svg>';
 
+		icoChat.innerHTML = '<svg width="24" height="24" viewBox="0 0 24 24"><path d="M15 16h4V6H5v10h7.33L15 18v-2zm2 2v2a1 1 0 01-1.6.8L11.67 18H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v10a2 2 0 01-2 2h-2z"/><path d="M7 10h10V8H7v2zm0 4h6v-2H7v2z"/></svg>';
 
 		let observer = new MutationObserver(mutationRecords => {
 
@@ -65,7 +67,16 @@
 
 			const btn = tooltip.querySelector('.tooltip-help__btn');
 
-			btn.appendChild(ico.cloneNode(true));
+			if(tooltip.classList.contains('tooltip-help--chat')) {
+
+				btn.appendChild(icoChat.cloneNode(true));
+
+			}
+			else {
+
+				btn.appendChild(ico.cloneNode(true));
+
+			}
 
 			observer.observe(tooltip, {
 
