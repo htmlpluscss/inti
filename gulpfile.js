34f83d6abc4eb4fff96b306343c6b72224101a5b
htmlpluscss <79198889134@ya.ru>
2021-01-30 10:28:04 +0300

    Ветка без запроса договора

--- gulpfile.js
+++ gulpfile.js
@@ -200,7 +200,7 @@ gulp.task('ftp', function () {
 gulp.task('watch', function() {
 	gulp.watch('src/js/*.*', gulp.series('js'));
 	gulp.watch('src/css/*.*', gulp.series('css'));
-	gulp.watch('src/**/index.html', gulp.series('html'));
+	gulp.watch('src/manager/**/index.html', gulp.series('html'));
 	gulp.watch(['src/_include/**/*.html','src/template/**/*.html'], gulp.series('html-touch'));
 	gulp.watch(['src/**/*.*', '!src/**/*.{css,html,js}'], gulp.series('copy'));
 	gulp.watch('build/**/*.*', gulp.series('ftp'));
