fe0da3f95e1e40d1ded27750fd7ebd5982c88b54
htmlpluscss <79198889134@ya.ru>
2021-05-07 18:14:35 +0300

    IntersectionObserver

--- src/js/circle-progress.js
+++ src/js/circle-progress.js
@@ -6,38 +6,58 @@
 
 	}
 
-	Array.from(elems, el => {
+	if ('IntersectionObserver' in window) {
 
-		const svg = el.querySelector('.circle-progress__svg'),
-			  text = el.querySelector('.circle-progress__value'),
-			  value = parseInt(text.textContent);
+		const callback = (entries, observer) => {
 
-		// анимация прогресса
-		const circle = svg.querySelector('circle'),
-			  pi2r = parseInt(circle.getAttribute('r')) * 2 * Math.PI;
+			Array.from(entries, entry => {
 
-		let count = 0;
+				if(entry.isIntersecting) {
 
-		if(value > 0) {
+					const target = entry.target;
 
-			el.classList.add('circle-progress--init');
+					observer.unobserve(target);
 
-			const idTimer = setInterval( () => {
+					const svg = target.querySelector('.circle-progress__svg'),
+						  text = target.querySelector('.circle-progress__value'),
+						  value = parseInt(text.textContent);
 
-				if(count === value) {
+					// анимация прогресса
+					const circle = svg.querySelector('circle'),
+						  pi2r = parseInt(circle.getAttribute('r')) * 2 * Math.PI;
 
-					clearInterval(idTimer);
+					let count = 0;
+
+					if(value > 0) {
+
+						target.classList.add('circle-progress--init');
+
+						const idTimer = setInterval( () => {
+
+							if(count === value) {
+
+								clearInterval(idTimer);
+
+							}
+
+							text.textContent = count++;
+
+							circle.setAttribute('stroke-dasharray', pi2r / 100 * count + ' ' + pi2r);
+
+						}, 1000 / value);
+
+					}
 
 				}
 
-				text.textContent = count++;
+			});
 
-				circle.setAttribute('stroke-dasharray', pi2r / 100 * count + ' ' + pi2r);
+		};
 
-			}, 1000 / value);
+		const observer = new IntersectionObserver(callback);
 
-		}
+		Array.from(elems, el => observer.observe(el));
 
-	});
+	}
 
 })(document.querySelectorAll('.circle-progress'));
--- src/js/line-progress.js
+++ src/js/line-progress.js
@@ -6,35 +6,55 @@
 
 	}
 
-	Array.from(elems, el => {
+	if ('IntersectionObserver' in window) {
 
-		const bar = el.querySelector('.line-progress__bar'),
-			  text = el.querySelector('.line-progress__value'),
-			  value = parseInt(text.textContent);
+		const callback = (entries, observer) => {
 
-		// анимация прогресса
-		let count = 0;
+			Array.from(entries, entry => {
 
-		if(value > 0) {
+				if(entry.isIntersecting) {
 
-			el.classList.add('line-progress--init');
+					const target = entry.target;
 
-			const idTimer = setInterval( () => {
+					observer.unobserve(target);
 
-				if(count === value) {
+					const bar = target.querySelector('.line-progress__bar'),
+						  text = target.querySelector('.line-progress__value'),
+						  value = parseInt(text.textContent);
 
-					clearInterval(idTimer);
+					// анимация прогресса
+					let count = 0;
+
+					if(value > 0) {
+
+						target.classList.add('line-progress--init');
+
+						const idTimer = setInterval( () => {
+
+							if(count === value) {
+
+								clearInterval(idTimer);
+
+							}
+
+							bar.style.width = count + '%';
+
+							text.textContent = count++;
+
+						}, 1000 / value);
+
+					}
 
 				}
 
-				bar.style.width = count + '%';
+			});
 
-				text.textContent = count++;
+		};
 
-			}, 1000 / value);
+		const observer = new IntersectionObserver(callback);
 
-		}
+		Array.from(elems, el => observer.observe(el));
 
-	});
+	}
 
 })(document.querySelectorAll('.line-progress'));
