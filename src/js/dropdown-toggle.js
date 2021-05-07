34edef3fde05f22fe97ed8f215087f4eeb56fe7a
htmlpluscss <79198889134@ya.ru>
2021-05-07 21:04:52 +0300

    Опыт поставок и референция

--- src/js/dropdown-toggle.js
+++ src/js/dropdown-toggle.js
@@ -6,6 +6,26 @@
 
 	}
 
+	Array.from(dropdowns, el => {
+
+		if(el.classList.contains('dropdown-toggle-radio')){
+
+			const current = el.querySelector('.dropdown-toggle-radio__current'),
+				  items = el.querySelectorAll('.dropdown-toggle-radio__item');
+
+			Array.from(items, item => {
+
+				const label = item.querySelector('.dropdown-toggle-radio__label'),
+					  input = item.querySelector('.dropdown-toggle-radio__input')
+
+				input.addEventListener("change", () => current.textContent = label.textContent);
+
+			});
+
+		}
+
+	});
+
 	window.addEventListener("click", event => {
 
 		const isDropdown = event.target.closest('.dropdown-toggle');
