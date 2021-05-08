f5fed8e7c0abf9f73c3b17b8a41bc97af1326ecb
htmlpluscss <79198889134@ya.ru>
2021-05-08 17:06:17 +0300

    Отзывы

---
+++ src/js/review-stat.js
@@ -0,0 +1,61 @@
+( elems => {
+
+	if(!elems.length) {
+
+		return;
+
+	}
+
+	if ('IntersectionObserver' in window) {
+
+		const callback = (entries, observer) => {
+
+			Array.from(entries, entry => {
+
+				if(entry.isIntersecting) {
+
+					const target = entry.target;
+
+					observer.unobserve(target);
+
+					const bar = document.createElement('span'),
+						  text = target.querySelector('.review-stat__details-value'),
+						  value = parseFloat(text.textContent) * 20; // 5.0 max, для 100% умножаем на 20
+
+					bar.className = 'review-stat__details-progress';
+					target.appendChild(bar);
+
+					// анимация прогресса
+					let count = 0;
+
+					if(value > 0) {
+
+						const idTimer = setInterval( () => {
+
+							count++;
+
+							if(count === value) {
+
+								clearInterval(idTimer);
+
+							}
+
+							bar.style.width = count + '%';
+
+						}, 1000 / value);
+
+					}
+
+				}
+
+			});
+
+		};
+
+		const observer = new IntersectionObserver(callback);
+
+		Array.from(elems.querySelectorAll('.review-stat__details-row'), el => observer.observe(el));
+
+	}
+
+})(document.querySelector('.review-stat'));
