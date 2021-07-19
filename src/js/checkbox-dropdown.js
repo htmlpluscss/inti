c5fe97f49eaa89244d4b4e267646b962be9d5d25
htmlpluscss <79198889134@ya.ru>
2021-07-19 12:17:59 +0300

    Каталог стандартов

--- src/js/checkbox-dropdown.js
+++ src/js/checkbox-dropdown.js
@@ -1,149 +1,29 @@
-( dropdowns => {
+( forms => {
 
-	if(!dropdowns.length) {
+	if(!forms.length) {
 
 		return;
 
 	}
 
-// склонение
-	const declension = (num, expressions) => {
+	Array.from(forms, form => {
 
-		let r,
-			count = num % 100;
+		const checkboxes = form.querySelectorAll('.checkbox__input:not([value="all"])'),
+			  checkboxAll = form.querySelector('.checkbox__input[value="all"]');
 
-		if (count > 4 && count < 21){
+		form.addEventListener("change", events => {
 
-			r = expressions['2'];
+			if(events.target === checkboxAll) {
 
-		}
-		else {
+				Array.from(checkboxes, checkbox => checkbox.checked = checkboxAll.checked);
 
-			count = count % 10;
+			} else {
 
-			if (count == 1){
-				r = expressions['0'];
-			}
-			else if (count > 1 && count < 5){
-				r = expressions['1'];
-			}
-			else{
-				r = expressions['2'];
-			}
+				let count = 0;
 
-		}
+				Array.from(checkboxes, checkbox => checkbox.checked ? count++ : '');
 
-		return r;
-
-	}
-
-	Array.from(dropdowns, el => {
-
-		const form = el.closest('form'),
-			  reset = document.createElement('button'),
-			  arrow = document.createElement('span'),
-			  btn = el.querySelector('.checkbox-dropdown__btn'),
-			  btnTextDefault = btn.textContent,
-			  expressions = btn.getAttribute('data-declension').split(','),
-			  inputList = el.querySelectorAll('.checkbox-dropdown__input');
-
-		arrow.className ='checkbox-dropdown__arrow';
-		arrow.innerHTML = '<svg width="24" height="24" viewBox="0 0 24 24"><path d="M8.3 10.3a1 1 0 000 1.41l2.93 2.97c.22.21.5.32.78.32s.56-.1.77-.32l2.93-2.96a1.01 1.01 0 00-.32-1.63.99.99 0 00-1.09.21L12 12.62l-2.3-2.33a.99.99 0 00-1.4 0z"/></svg>';
-
-		reset.setAttribute('type','button');
-		reset.className = 'checkbox-dropdown__reset button hide';
-		reset.innerHTML = '<svg width="24" height="24" viewBox="0 0 24 24"><path d="M12 10.59l-5.3-5.3a1 1 0 00-1.4 1.42L10.58 12l-5.3 5.3a1 1 0 001.42 1.4L12 13.42l5.3 5.3a1 1 0 001.4-1.42L13.42 12l5.3-5.3a1 1 0 10-1.42-1.4L12 10.58z"/></svg>';
-
-		el.appendChild(arrow);
-		el.appendChild(reset);
-
-		const Reset = () => {
-
-			arrow.classList.remove('hide');
-			reset.classList.add('hide');
-
-			btn.textContent = btnTextDefault;
-			el.classList.remove('checkbox-dropdown--checked');
-
-			Array.from(inputList, input => {
-
-				input.checked = false;
-
-			});
-
-		}
-
-		form && form.addEventListener("reset", () => Reset());
-		reset.addEventListener("click", () => Reset());
-
-		Array.from(inputList, input => {
-
-			input.addEventListener("change", () => {
-
-				let countChecked = 0,
-					textChecked;
-
-				Array.from(inputList, input => {
-
-					if(input.checked) {
-
-						countChecked++;
-						textChecked = input.nextElementSibling.textContent;
-
-					}
-
-				});
-
-				if(countChecked === 0) {
-
-					btn.textContent = btnTextDefault;
-					el.classList.remove('checkbox-dropdown--checked');
-
-				}
-				else if(countChecked === 1) {
-
-					arrow.classList.remove('hide');
-					reset.classList.add('hide');
-					btn.textContent = textChecked;
-					el.classList.add('checkbox-dropdown--checked');
-
-				}
-				else {
-
-					arrow.classList.add('hide');
-					reset.classList.remove('hide');
-					btn.textContent = countChecked + ' ' + declension(countChecked, expressions);
-
-				}
-
-			});
-
-		});
-
-	});
-
-
-//	data-declension
-
-
-	window.addEventListener("click", event => {
-
-		const isDropdown = event.target.closest('.checkbox-dropdown');
-
-		Array.from(dropdowns, el => {
-
-			if(el === isDropdown){
-
-				if(event.target.closest('.checkbox-dropdown__btn')) {
-
-					el.classList.toggle('checkbox-dropdown--open');
-
-				}
-
-			}
-			else {
-
-				el.classList.remove('checkbox-dropdown--open');
+				checkboxAll.checked = checkboxes.length === count;
 
 			}
 
@@ -151,4 +31,4 @@
 
 	});
 
-})(document.querySelectorAll('.checkbox-dropdown'));
+})(document.querySelectorAll('.checkbox-list-all'));
