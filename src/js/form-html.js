8115b8b2af9fcf4fe29a636fd903ab46adfd6882
htmlpluscss <79198889134@ya.ru>
2021-09-01 10:38:14 +0300

    lang

--- src/js/form-html.js
+++ src/js/form-html.js
@@ -34,3 +34,37 @@
 	});
 
 })(document.querySelectorAll('.form-html'));
+
+
+( items => {
+
+	if(!items.length) {
+
+		return;
+
+	}
+
+	Array.from(items, form => {
+
+		const resultContainer = document.querySelector('#' + form.getAttribute('data-result'));
+
+		form.addEventListener('change', () => {
+
+			fetch(form.getAttribute("action"), {
+				method: "POST",
+				body: new FormData(form)
+			})
+			.then(response => response.text())
+			.then(result => {
+
+				console.log(result);
+
+				resultContainer.innerHTML = result;
+
+			});
+
+		});
+
+	});
+
+})(document.querySelectorAll('.form-html-formdata'));
