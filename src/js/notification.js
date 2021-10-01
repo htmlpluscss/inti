8bb63cd9c5d9b43e4106fe66dba4abdbbf9108cd
htmlpluscss <79198889134@ya.ru>
2021-10-01 20:39:11 +0300

    уведомления

---
+++ src/js/notification.js
@@ -0,0 +1,33 @@
+( notification => {
+
+	if(!notification) {
+
+		return;
+
+	}
+
+	let resizeTimeout = null;
+
+	notification.addEventListener('click', () => {
+
+		clearTimeout(resizeTimeout);
+		notification.classList.remove('is-show');
+
+	});
+
+	window.notification = (head, text, timer = 5) => {
+
+		notification.querySelector('.notification__head').innerHTML = head ? head : '';
+		notification.querySelector('.notification__text').innerHTML = text ? text : '';
+
+		notification.classList.add('is-show');
+
+		resizeTimeout = setTimeout( ()=> {
+
+			notification.classList.remove('is-show');
+
+		}, 1000 * timer);
+
+	}
+
+})(document.querySelector('.notification'));
