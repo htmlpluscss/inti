f781ba064e70890cad2fd9969927154f33d53814
htmlpluscss <79198889134@ya.ru>
2021-10-11 16:46:11 +0300

    select

---
+++ src/js/select.js
@@ -0,0 +1,126 @@
+window.selects = select => {
+
+	if(select.querySelector('.select__list')) {
+
+		return;
+
+	}
+
+	const value = document.createElement('div'),
+		  arrow = document.createElement('span');
+
+	arrow.innerHTML = '<svg width="29" height="24" viewBox="0 0 29 24"><path d="m9.85 9 4.5 5 4.5-5h-9Z"/></svg>';
+
+	value.className = 'select__value';
+	value.innerHTML = '<span class="select__value-inner"></span>';
+
+	value.appendChild(arrow);
+	select.appendChild(value);
+
+	const form = select.closest('form'),
+		control = select.querySelector('select'),
+		option = select.querySelectorAll('option'),
+		valueText = select.querySelector('.select__value-inner'),
+		list = document.createElement('div');
+
+	list.className = 'select__list';
+
+	let selected = control.querySelector('[value="' + control.value + '"]');
+
+	control.addEventListener("change", () => {
+
+		selected = control.querySelector('[value="' + control.value + '"]');
+
+		valueText.textContent = selected.textContent;
+
+		select.classList.remove('select--default');
+
+	});
+
+	const valueDefault = selected.textContent;
+
+	if(control.disabled){
+
+		select.classList.add('select--disabled');
+
+	}
+
+	if(control.value === 'none' || control.value === ''){
+
+		select.classList.add('select--default');
+
+	}
+
+	if (control.value !== '') {
+
+		valueText.textContent = select.querySelector('option[value="' + control.value + '"]').textContent;
+
+	}
+
+	Array.from(option, el => {
+
+		const o = document.createElement('button');
+
+		o.className = 'button select__list-item';
+
+		o.setAttribute('type', 'button');
+		o.setAttribute('value', el.getAttribute('value'));
+
+		o.textContent = el.textContent;
+
+		list.appendChild(o);
+
+	});
+
+	select.addEventListener("click", event => {
+
+		if(event.target.classList.contains('select__list-item')){
+
+			control.value = event.target.getAttribute('value');
+
+			control.dispatchEvent(new CustomEvent("change"));
+
+			if(form) {
+
+				form.dispatchEvent(new CustomEvent("change"));
+
+			}
+
+		}
+
+	});
+
+	select.appendChild(list);
+
+	form && form.addEventListener("reset", () => {
+
+		select.classList.add('select--default');
+		valueText.textContent = valueDefault;
+
+	});
+
+};
+
+( () => {
+
+	window.selectsCollection = document.querySelectorAll('.select');
+
+	if(window.selectsCollection.length) {
+
+		Array.from(window.selectsCollection, select => window.selects(select));
+
+	}
+
+	window.addEventListener("click", event => {
+
+		const isSelect = event.target.closest('.select');
+
+		Array.from(window.selectsCollection, select => {
+
+			select.classList.toggle('select--open', select === isSelect && isSelect.classList.contains('select--open') === false);
+
+		});
+
+	});
+
+})();
