0998938b2c28338c8bcb4646fc470c826107af2e
htmlpluscss <79198889134@ya.ru>
2021-10-21 17:31:04 +0300

    insights

---
+++ src/js/insights.js
@@ -0,0 +1,38 @@
+
+( page => {
+
+	if(page) {
+
+		const box = page.querySelector('.insights-page__box');
+
+		// кнопка каталог
+
+		window.addEventListener("click", event => {
+
+			if( event.target.closest('.insights-category-list') === null ){
+
+				if( event.target.closest('.insights-page__btn-catalog') ){
+
+					box.classList.toggle('is-open-category');
+
+				} else {
+
+					box.classList.remove('is-open-category');
+
+				}
+
+			}
+
+			// в каталоге развернуть списки
+
+			if( event.target.closest('.insights-category-list__item') && event.target.closest('.insights-category-list__link') === null ) {
+
+				event.target.closest('.insights-category-list__item').classList.toggle('is-open');
+
+			}
+
+		});
+
+	}
+
+})(document.querySelector('.insights-page'));
