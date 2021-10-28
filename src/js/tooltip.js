20c94956d335042af7ff48de68cd5f892192299e
htmlpluscss <79198889134@ya.ru>
2021-10-28 16:19:12 +0300

    quality

--- src/js/tooltip.js
+++ src/js/tooltip.js
@@ -17,14 +17,17 @@
 	}
 
 })(document.querySelectorAll('.tooltip-title'));
-
+*/
 ( tooltips => {
 
 	if(tooltips.length){
 
-		const ico = document.createElement('span');
+		const ico = document.createElement('span'),
+			  icoAchtung = document.createElement('span');
 
 		ico.innerHTML = '<svg viewBox="0 0 24 24"><path fill-rule="evenodd" d="M20 12a8 8 0 11-16 0 8 8 0 0116 0zm-5.74-3.87a3.18 3.18 0 01.56 3.78c-.38.7-1 1.22-1.74 1.49a.4.4 0 00-.28.38v.62a.8.8 0 01-1.6 0V12.8A.8.8 0 0112 12c.88 0 1.6-.72 1.6-1.6a1.6 1.6 0 00-3.2 0 .8.8 0 01-1.59.08l-.01-.12c.01-2 1.86-3.55 3.95-3.07.57.13 1.1.42 1.51.84zM12.8 16.8a.8.8 0 11-1.6 0 .8.8 0 011.6 0z"/></svg>';
+
+		icoAchtung.innerHTML = '<svg width="12" height="12" viewBox="0 0 12 12"><path fill-rule="evenodd" d="M11 6A5 5 0 1 1 1 6a5 5 0 0 1 10 0ZM5 7V2h2v5H5Zm2.03 1h-2v2H7l.03-2Z"/></svg>';
 
 		let observer = new MutationObserver(mutationRecords => {
 
@@ -61,7 +64,15 @@
 
 			const btn = tooltip.querySelector('.tooltip-help__btn');
 
-			btn.appendChild(ico.cloneNode(true));
+			if ( tooltip.classList.contains('tooltip-help--achtung') ) {
+
+				btn.appendChild(icoAchtung.cloneNode(true));
+
+			} else {
+
+				btn.appendChild(ico.cloneNode(true));
+
+			}
 
 			observer.observe(tooltip, {
 
@@ -89,4 +100,4 @@
 
 	}
 
-})(document.querySelectorAll('.tooltip-help'));*/
+})(document.querySelectorAll('.tooltip-help'));
