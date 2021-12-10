1bf22c9fa31e21bf917a3324b8562000292a7ca7
htmlpluscss <79198889134@ya.ru>
2021-12-10 11:43:53 +0300

    lang

---
+++ src/js/yatranslate.js
@@ -0,0 +1,41 @@
+( btns => {
+
+	if( btns.length === 0 ) {
+
+		return;
+
+	}
+
+	if ( Cookies.get('lang') === 'en' ) {
+
+		const ytWidget = document.createElement('div');
+		ytWidget.id = 'ytWidget';
+		ytWidget.style.display = 'none';
+
+		document.body.appendChild(ytWidget);
+
+		const script = document.createElement('script');
+		script.src = "https://translate.yandex.net/website-widget/v1/widget.js?widgetId=ytWidget&pageLang=ru&widgetTheme=light&autoMode=false";
+
+		document.head.appendChild(script);
+
+	}
+
+	Array.from(btns, el => {
+
+		el.addEventListener('click', () => {
+
+			localStorage.setItem('yt-widget', JSON.stringify({
+				"lang": el.getAttribute('data-lang'),
+				"active": true
+			}));
+
+			Cookies.set('lang', el.getAttribute('data-lang'));
+
+			location.reload();
+
+		});
+
+	});
+
+})(document.querySelectorAll('.header__lang-btn'));
