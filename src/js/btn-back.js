4b8db801121e5722640b876df1d0f974f314aaf1
htmlpluscss <79198889134@ya.ru>
2021-12-11 07:34:47 +0300

    header

---
+++ src/js/btn-back.js
@@ -0,0 +1,25 @@
+
+( links => {
+
+	if(links.length) {
+
+		const historyBack = event=> {
+
+			event.preventDefault();
+			history.back();
+
+		}
+
+		Array.from(links, link => {
+
+			if(document.referrer.indexOf(location.hostname) > 0) {
+
+				link.onclick = historyBack;
+
+			}
+
+		});
+
+	}
+
+})(document.querySelectorAll('.js-btn-back'));
