299e2fec453080859da6fcf88513c8a7440b6e56
htmlpluscss <79198889134@ya.ru>
2021-12-16 11:22:43 +0300

    Чат

--- src/js/workgroup-chat.js
+++ src/js/workgroup-chat.js
@@ -6,15 +6,36 @@
 
 	}
 
-	const form = item.querySelector('.workgroup-chat-form');
+	const formGroup = item.querySelector('.workgroup-chat-form-group');
 
-	if ( form ) {
+	if ( formGroup ) {
 
-		const btn = form.querySelector('.workgroup-chat-form__submit'),
+		// переключение
+
+		const btns = formGroup.querySelectorAll('.workgroup-chat-form-group__btn');
+
+		Array.from(btns, btn => {
+
+			btn.addEventListener('click', () => {
+
+				const id = btn.getAttribute('data-item');
+
+				formGroup.classList.remove('is-chat','is-message','is-user');
+				formGroup.classList.add('is-' + id);
+
+				id === 'chat' ? filterReset() : filterFocus();
+
+			});
+
+		});
+
+		// отправка сообщений
+
+		const form = formGroup.querySelector('.workgroup-chat-form'),
+			  btn = form.querySelector('.workgroup-chat-form__submit'),
 			  input = form.querySelector('.workgroup-chat-form__input'),
 			  file = form.querySelector('.workgroup-chat-form__input-file input'),
-			  list = form.querySelector('.workgroup-chat__list'),
-			  empty = form.querySelector('.workgroup-chat__empty');
+			  list = form.querySelector('.workgroup-chat__list');
 
 		const send = ()=> {
 alert('в ответе ждум html .workgroup-chat__item');
@@ -35,12 +56,6 @@ return;
 				btn.disabled = false;
 
 				list.insertAdjacentHTML('afterbegin', result);
-
-				if ( empty ) {
-
-					empty.remove();
-
-				}
 
 			});
 
@@ -67,6 +82,53 @@ return;
 				file.value = '';
 
 			}
+
+		});
+
+		// фильтр чата
+
+		const filter = formGroup.querySelector('.workgroup-chat-filter');
+
+		const filterReset = ()=> {
+console.log('reset')
+			filter.classList.add('is-empty');
+			filterFocus();
+
+			Array.from(document.querySelectorAll('.workgroup-chat-item__filter-item'), item => item.classList.remove('hide'));
+
+		};
+
+		const filterFocus = ()=> {
+
+			Array.from(filter.querySelectorAll('.input'), input => {
+
+			if ( input.offsetParent !== null )
+
+				input.focus();
+
+			});
+
+		};
+
+		filter.addEventListener('submit', event => {
+
+			event.preventDefault();
+
+		});
+
+		filter.addEventListener('reset', filterReset);
+
+		filter.addEventListener('input', event => {
+
+			const value = event.target.value;
+
+			filter.classList.toggle('is-empty', value.length === 0);
+
+			Array.from(document.querySelectorAll('.workgroup-chat-item__filter-' + event.target.name), item => {
+
+				item.closest('.workgroup-chat-item__filter-item').classList.toggle('hide', item.textContent.toLowerCase().indexOf(value.toLowerCase()) === -1);
+
+			});
 
 		});
 
